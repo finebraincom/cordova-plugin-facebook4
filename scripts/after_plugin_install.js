aa05b421abe2c532db9a206b7f4eaf7dc83263a1
Iurii Kyian <iurii.kyian@insights.md>
2017-10-23 15:45:12 +0200

    plugin hook updated to correctly work with cordova 7

---
+++ scripts/after_plugin_install.js
@@ -0,0 +1,46 @@
+
+function _configureWeb(){
+    'use strict';
+    var fs = require('fs');
+    function getPreferenceValue(config, name) {
+        var value = config.match(new RegExp('name="' + name + '" value="(.*?)"', "i"));
+        if(value && value[1]) {
+            return value[1];
+        } else {
+            return null;
+        }
+    }
+
+    var APP_ID;
+    if(process.argv.join("|").indexOf("APP_ID=") > -1) {
+    	APP_ID = process.argv.join("|").match(/APP_ID=(.*?)(\||$)/)[1]
+    } else {
+    	var config = fs.readFileSync("config.xml").toString()
+    	APP_ID = getPreferenceValue(config, "APP_ID");
+    }
+
+    var files = [
+        "platforms/browser/www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "platforms/browser/platform_www/plugins/cordova-plugin-facebook4/www/facebook-browser.js",
+        "platforms/browser/www/cordova.js",
+        "platforms/browser/platform_www/cordova.js"
+    ];
+
+    for(var i in files) {
+        try {
+        	var contents = fs.readFileSync(files[i]).toString()
+    	    fs.writeFileSync(files[i], contents.replace(/APP_ID/g, APP_ID));
+    	} catch(err) {}
+    }
+}
+
+module.exports = function(context){
+
+    var Q = context.requireCordovaModule('q');
+    var deferral = new Q.defer();
+
+    _configureWeb();
+    deferral.resolve();
+
+    return deferral.promise;
+};
